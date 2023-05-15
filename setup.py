dd069b0abb7204dbbafc98375e52593d5a45aa18
AlessandroDalponte <adalponte0@gmail.com>
2023-05-15 18:02:01 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# from distutils.core import setup
+from setuptools import find_packages
+from setuptools import setup 
+
+setup(name='face_detector',
+      version='1.0',
+      description='Retail Intelligence face_detector',
+      author='nautec',
+      packages=find_packages(),
+      url='https://github.com/retail-intelligence/Ultra-Light-Fast-Generic-Face-Detector-1MB',
+    )
