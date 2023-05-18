216b8e4ed97fd5fa2c31a77c44e7e419c8929655
AlessandroDalponte <adalponte0@gmail.com>
2023-05-18 11:16:26 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,14 @@ setup(name='facedetector',
       version='1.0',
       description='Retail Intelligence face_detector',
       author='nautec',
+      install_requires=[
+      'numpy>=1.24.2',
+      'torch>=1.12.1',
+      'torchvision>=0.13.1',
+      'opencv_python>=4.7.0.72',
+      'matplotlib>=3.5.3',
+      'wget>=3.2'
+      ],
       packages=find_packages(),
       url='https://github.com/retail-intelligence/Ultra-Light-Fast-Generic-Face-Detector-1MB',
     )
